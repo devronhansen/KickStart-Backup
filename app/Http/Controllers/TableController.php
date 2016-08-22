9b068ea2bd1725b3cf6771143595e7ee7b20234e
Tsumef4n <tsumef4n@yahoo.de>
2016-08-22 11:33:04 +0200

    Tabs und Table ohne Content

--- app/Http/Controllers/TableController.php
+++ app/Http/Controllers/TableController.php
@@ -13,9 +13,21 @@ class TableController extends Controller
         //SQL Code?
 
         //Arrays fuellen
+        $news = array(
+            array('id' => '1', 'news_text' => 'Hallo Welt! Ich bin gaaaaaaaylord McFickster'),
+            array('id' => '2', 'news_text' => 'Ich bin auch ne News, voll cool und so'),
+        );
 
+        //echo "<pre>";
+        //print_r(array_values($news));
+        //echo "</pre>";
 
         //Arrays mit uebergeben
-        return view('home');
+        return view('home', [
+            'news' => $news,
+            'test_text' => "ich bin ein test",
+            'test_id' => "1",
+            'test_header' => "huhu",
+        ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,13 +4,47 @@
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+            <ul class="nav nav-tabs">
+              <li class="active"><a data-toggle="tab" href="#news">Home</a></li>
+              <li><a data-toggle="tab" href="#menu1">Menu 1</a></li>
+              <li><a data-toggle="tab" href="#menu2">Menu 2</a></li>
+            </ul>
 
-                <div class="panel-body">
-                    You are logged in!
-                </div>
+            <div class="tab-content">
+              <div id="news" class="tab-pane fade in active">
+                  <!-- Table News-->
+                  <div class="container col-md-12">
+                    <table class="table table-striped table-hover">
+                      <thead>
+                        <tr>
+                          <th>ID</th>
+                          <th>Header</th>
+                          <th>Text</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+
+                        <tr>
+                          <td> {{ $test_id }} </td>
+                          <td> {{ $test_header }} </td>
+                          <td> {{ $test_text }} </td>
+                        </tr>
+
+                      </tbody>
+                    </table>
+                  </div>
+
+              </div>
+              <div id="menu1" class="tab-pane fade">
+                <h3>Menu 1</h3>
+                <p>Some content in menu 1.</p>
+              </div>
+              <div id="menu2" class="tab-pane fade">
+                <h3>Menu 2</h3>
+                <p>Some content in menu 2.</p>
+              </div>
             </div>
+
         </div>
     </div>
 </div>
