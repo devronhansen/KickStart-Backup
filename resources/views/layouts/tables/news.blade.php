82acd866987a1c22c0e42fb24a1a6318129f484d
Tsumef4n <tsumef4n@yahoo.de>
2016-08-23 07:48:18 +0200

    tabstop

--- resources/views/layouts/tables/news.blade.php
+++ resources/views/layouts/tables/news.blade.php
@@ -29,8 +29,8 @@
                <form method="POST">
                   <div class="modal" id="model-{{ $id }}" tabindex="" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                       <div class="modal-dialog">
-                          <div class="modal-content">
-                              <div class="modal-header">
+                        <div class="modal-content">
+                           <div class="modal-header">
                                   <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                   <h4 class="modal-title" id="myModalLabel">{{$user->title}}</h4>
                               </div>
